90a5ed8cafe935aba42d4f84eee407aefc19552c
Paul Richards <paulware@hotmail.com>
2020-01-08 08:53:22 -0600

    comment

--- utilities/fillSand.js
+++ utilities/fillSand.js
@@ -9,22 +9,39 @@ exports.fillSand = function () {
   var inhand;
   var name;
   var player;
-  exports.player = self;
+  var block;
   events.projectileHit( function (event) {
     projectile=event.getEntity();
     location=projectile.location;0
     if ((projectile.getType()) == (org.bukkit.entity.EntityType.SNOWBALL)){
+      block = server.worlds[0].getBlockAt (location);	  
       x=parseInt(location.x);
-      y=parseInt(location.y) -1;
+      y=parseInt(location.y);
+      z=parseInt(location.z)-1;
+	  
+	  blockType=block.getType();
+	  console.log ( 'snowball collided with block: [' + blockType + ']' )
+      if (blockType != org.bukkit.Material.AIR){
+		 y = y +1;
+	  } else {
+		 block = server.worlds[0].getBlockAt (location.add (0,-1,0)); // block below
+		 blockType = block.getType();
+		 if (blockType == org.bukkit.Material.AIR) {
+			 y = y - 1;
+		 }
+			 
+	  }
+
+	  /*
+	  console.log ( 'exports.player.name: ' + exports.player.name );
       inhand=exports.player.getItemInHand();
       name=(inhand.getItemMeta() == null) ? null : inhand.getItemMeta().getDisplayName();
 	  console.log ( 'inhand: [' + name + ']')
       if (name== "Sand"){
 		y = y + 1;
 	  }
-	  
-      z=parseInt(location.z) -1;
-      cmd="fill " + (x+10) + " " + y + " " + z  + " " + (x-10) + " " + y + " " + z + " sand keep";
+	  */
+      cmd="fill " + x + " " + y + " " + z  + " " + (x-10) + " " + y + " " + z + " sand keep";
       org.bukkit.Bukkit.dispatchCommand(org.bukkit.Bukkit.getConsoleSender(), cmd);
     }
   });
