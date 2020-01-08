686ced6d660809a17b8e26266a398266e28da320
Paul Richards <paulware@hotmail.com>
2020-01-07 23:29:01 -0600

    comment

--- utilities/fillSand.js
+++ utilities/fillSand.js
@@ -6,12 +6,20 @@ exports.fillSand = function () {
   var y;
   var z;
   var cmd;
+  var inhand;
+  var name;
   events.projectileHit( function (event) {
     projectile=event.getEntity();
-    location=projectile.location;
+    location=projectile.location;0
     if ((projectile.getType()) == (org.bukkit.entity.EntityType.SNOWBALL)){
       x=parseInt(location.x);
-      y=parseInt(location.y)  -1;
+      y=parseInt(location.y) -1;
+      inhand=player.getItemInHand();
+      name=(inhand.getItemMeta() == null) ? null : inhand.getItemMeta().getDisplayName();
+      if (name== "sand"){
+		y = y + 1;
+      }
+	  
       z=parseInt(location.z) -1;
       cmd="fill " + (x+10) + " " + y + " " + z  + " " + (x-10) + " " + y + " " + z + " sand keep";
       org.bukkit.Bukkit.dispatchCommand(org.bukkit.Bukkit.getConsoleSender(), cmd);
