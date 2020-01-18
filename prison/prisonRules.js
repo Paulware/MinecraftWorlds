dc90f75bd2b31af7f5c86a6b2322fa8ac803b0d2
Paulware <Paulware@hotmail.com>
2020-01-17 21:05:01 -0600

    comment

--- prison/prisonRules.js
+++ prison/prisonRules.js
@@ -1,15 +1,58 @@
 exports.prisonRules = function () {
-  org.bukkit.Bukkit.dispatchCommand(org.bukkit.Bukkit.getConsoleSender(), "gamemode survival @a");
+  //Instantiations;
+  var player;
+  var inhand;
+  var name;
+  var i;
+  var projectile;
+  var shooter;
   org.bukkit.Bukkit.dispatchCommand(org.bukkit.Bukkit.getConsoleSender(), "weather clear");
   org.bukkit.Bukkit.dispatchCommand(org.bukkit.Bukkit.getConsoleSender(), "time set night");
   org.bukkit.Bukkit.dispatchCommand(org.bukkit.Bukkit.getConsoleSender(), "gamerule doWeatherCycle false");
   org.bukkit.Bukkit.dispatchCommand(org.bukkit.Bukkit.getConsoleSender(), "gamerule doDaylightCycle false");
-  org.bukkit.Bukkit.dispatchCommand(org.bukkit.Bukkit.getConsoleSender(), "spawnpoint @a -1219 137 -91");
+  org.bukkit.Bukkit.dispatchCommand(org.bukkit.Bukkit.getConsoleSender(), "spawnpoint @a 121 87 1139");
   org.bukkit.Bukkit.dispatchCommand(org.bukkit.Bukkit.getConsoleSender(), "deop @a");
   org.bukkit.Bukkit.dispatchCommand(org.bukkit.Bukkit.getConsoleSender(), "tp @a 121 87 1139");
   org.bukkit.Bukkit.dispatchCommand(org.bukkit.Bukkit.getConsoleSender(), "op " + self.name);
-  players = server.getOnlinePlayers();
-  for (var playersIndex=0; playersIndex<players.length; playersIndex++) {
-    players[playersIndex].getInventory().clear();
-  }
+  // spawn org.bukkit.entity.EntityType.WITHER
+  var location = new org.bukkit.Location(server.worlds[0], 110, 170, 1151);
+  var entity = server.worlds[0].spawnEntity(location,org.bukkit.entity.EntityType.WITHER);
+  events.playerInteract( function (event) {
+    player=event.getPlayer();
+    inhand=(player== null) ? null : ( player.getItemInHand == null) ? null : player.getItemInHand();
+    name=(inhand== null) ? null : (inhand.getItemMeta == null) ? null : (inhand.getItemMeta() == null)?null:inhand.getItemMeta().getDisplayName();
+    if ((name) == "minigun"){
+      console.log ("Handle minigun");
+      i=0;
+      var test= setInterval (function () {
+        i=i+1;
+        projectile=server.worlds[0].spawnEntity(player.location,org.bukkit.entity.EntityType.ARROW);
+        player.launchProjectile(projectile.getClass());
+        if (!(i<20)) {
+          clearInterval (test);
+        }
+      }, 150);
+    }
+    else if ((name) == "M1-Garand"){
+      console.log ("Handle M1-Garand");
+      projectile=server.worlds[0].spawnEntity(player.location,org.bukkit.entity.EntityType.ARROW);
+      player.launchProjectile(projectile.getClass());
+    }
+    else {
+      console.log ("Handle: [" + name + "]");
+    }
+  });
+  events.projectileLaunch( function (event) {
+    projectile=event.getEntity();
+    shooter=(projectile == null) ? null : (projectile.getShooter == null) ? null : projectile.getShooter();
+    inhand=(shooter== null) ? null : ( shooter.getItemInHand == null) ? null : shooter.getItemInHand();
+    if ((inhand) == "M1-Garand"){
+      (function() {
+        var vector = projectile.getVelocity().normalize().multiply(7);
+        if (!isNaN(vector.x)) {
+           projectile.setVelocity (vector);
+        }
+       })();
+    }
+  });
 };
