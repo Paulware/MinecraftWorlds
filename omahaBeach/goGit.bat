723d978deb0a7d20dc6e9bbb9814cb4e198e1764
Paulware <Paulware@hotmail.com>
2020-01-17 21:47:42 -0600

    comment

--- omahaBeach/goGit.bat
+++ omahaBeach/goGit.bat
@@ -1,7 +1,8 @@
-copy C:\SpigotMC\scriptcraft\plugins\omahaRules.js
-copy C:\SpigotMC\scriptcraft\plugins\xml\omahaRules.xml
+copy C:\SpigotMC\scriptcraft\plugins\prisonRules.js
+copy C:\SpigotMC\scriptcraft\plugins\xml\prisonRules.xml
 copy C:\SpigotMC\spigot.yml
 copy C:\SpigotMC\bukkit.yml
+copy C:\SpigotMC\server.properties
 cd ..
 git add .
 git commit -am "comment"
--- omahaBeach/readme.txt
+++
@@ -1,5 +0,0 @@
-Boat at -1223 62 -505
-Bunker at -1224 83 -412
-Lobby at -1219 135 -91
-
-startGame.js will force players to respawn and go to lobby.  This occurs on reload
