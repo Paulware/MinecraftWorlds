cc56785a15d2d8738617e938cacb910670d7bf75
Paulware <Paulware@hotmail.com>
2020-01-22 20:23:30 -0600

    comment

--- omahaBeach/goGit.bat
+++ omahaBeach/goGit.bat
@@ -2,7 +2,6 @@ copy C:\SpigotMC\scriptcraft\plugins\omahaRules.js
 copy C:\SpigotMC\scriptcraft\plugins\xml\omahaRules.xml
 copy C:\SpigotMC\spigot.yml
 copy C:\SpigotMC\bukkit.yml
-copy C:\SpigotMC\server.properties
 cd ..
 git add .
 git commit -am "comment"
