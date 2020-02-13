b9e13fbcbef46f54387554f234cf91f7cdd716e5
Paulware <Paulware@hotmail.com>
2020-02-13 14:30:52 -0600

    comment

--- prison/goGit.bat
+++ prison/goGit.bat
@@ -1,5 +1,5 @@
-copy C:\SpigotMC\scriptcraft\plugins\prisonRules.js
-copy C:\SpigotMC\scriptcraft\plugins\xml\prisonRules.xml
+copy C:\SpigotMC\scriptcraft\plugins\prison.js
+copy C:\SpigotMC\scriptcraft\plugins\xml\prison.xml
 copy C:\SpigotMC\spigot.yml
 copy C:\SpigotMC\bukkit.yml
 copy C:\SpigotMC\server.properties
