8d5dd6845ea125a917b7b2757b639fbb0b6393fa
Paulware <Paulware@hotmail.com>
2020-02-23 01:35:20 -0600

    comment

--- battlefield/battlefield.js
+++ battlefield/battlefield.js
@@ -46,4 +46,52 @@ exports.teamBeacon  = function (location) {
 };
 
 exports.battlefield = function () {
+  //Instantiations;
+  var objective;
+  var beacons;
+  var x;
+  var y;
+  var z;
+  var block;
+  var blockType;
+  var value;
+  exports.redScore=0;
+  exports.blueScore=0;
+  var manager = org.bukkit.Bukkit.getScoreboardManager();
+  exports.board = manager.getNewScoreboard();
+  var objective = exports.board.registerNewObjective("objective1", "HEALTH", "Scoreboard");
+  objective.setDisplaySlot(org.bukkit.scoreboard.DisplaySlot.SIDEBAR);
+  objective = exports.board.getObjective (org.bukkit.scoreboard.DisplaySlot.SIDEBAR);
+  objective.setDisplayName("Battlefield Red Team: 0  Blue Team: 0");
+  objective = exports.board.getObjective (org.bukkit.scoreboard.DisplaySlot.SIDEBAR);
+  objective.getScore(self).setScore(0);
+  self.setScoreboard (exports.board);
+  beacons=[[552,101,-1115]];
+  var yo= setInterval (function () {
+    for (var i=0; i<beacons.length;i++) {
+      x=beacons[i][0];
+      y=beacons[i][1] + 1;
+      z=beacons[i][2];
+      block=server.worlds[0].getBlockAt (new org.bukkit.Location(server.worlds[0], x, y, z));
+      blockType=(block==null)?null:block.getType();
+      if (((blockType) == (org.bukkit.Material.BLUE_STAINED_GLASS))){
+        (function () {
+          var value = ( exports.blueScore==null)?0:exports.blueScore;
+          exports.blueScore= value+1;
+        })();
+      }
+      else if (((blockType) == (org.bukkit.Material.RED_STAINED_GLASS))){
+        (function () {
+          var value = ( exports.redScore==null)?0:exports.redScore;
+          exports.redScore= value+1;
+        })();
+      }
+      objective = exports.board.getObjective (org.bukkit.scoreboard.DisplaySlot.SIDEBAR);
+      objective.setDisplayName("Battlefield Red Team:" + exports.redScore + "  Blue Team: " + exports.blueScore);
+      console.log (blockType);
+    };
+    if (!(true)) {
+      clearInterval (yo);
+    }
+  }, 3000);
 };
