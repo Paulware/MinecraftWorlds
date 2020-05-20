add9802b12856d2e54e6e98dc8d4b144d6f7ddac
Paulware <Paulware@hotmail.com>
2020-05-19 21:06:57 -0500

    comment

--- spleef/spleef.js
+++ spleef/spleef.js
@@ -1,87 +1,105 @@
 exports.spleef  = function () {
   //Instantiations;
-  var objective;
   var player;
+  var objective;
   var players;
+  var block;
+  var teamColor;
   var score;
   var value;
-  var block;
-  if (((exports.gameStarted) == (null))){
-    exports.gameStarted=1;
-    var manager = org.bukkit.Bukkit.getScoreboardManager();
-    exports.board = manager.getNewScoreboard();
-    var objective = exports.board.registerNewObjective("objective1", "HEALTH", "Scoreboard");
-    objective.setDisplaySlot(org.bukkit.scoreboard.DisplaySlot.SIDEBAR);
-    objective = exports.board.getObjective (org.bukkit.scoreboard.DisplaySlot.SIDEBAR);
-    objective.setDisplayName("Welcome to Spleef yo");
-    org.bukkit.Bukkit.dispatchCommand(org.bukkit.Bukkit.getConsoleSender(), "setworldspawn -87 14 132");
-    org.bukkit.Bukkit.dispatchCommand(org.bukkit.Bukkit.getConsoleSender(), "setspawn -87 14 132");
-    events.playerDeath( function (event) {
-      player=(event.getPlayer== null) ? null : event.getPlayer();
+  events.playerJoin( function (event) {
+    player=(event.getPlayer== null) ? null : event.getPlayer();
+    console.log (player.name + ' now joining the spleef game');
+    fd = new org.bukkit.metadata.FixedMetadataValue (__plugin,0);
+    if (player != null) {
+      if (player.setMetadata != null ) {
+        player.setMetadata ("_score_", fd );
+      }
+    }
+    setTimeout (function () {
+      player.teleport(new org.bukkit.Location(server.worlds[0], -73, 29, 80), org.bukkit.event.player.PlayerTeleportEvent.TeleportCause.PLUGIN);
+    },2000);
+    player.setGameMode(org.bukkit.GameMode.SURVIVAL);
+    org.bukkit.Bukkit.dispatchCommand(org.bukkit.Bukkit.getConsoleSender(), "effect give " + player.name + " instant_health 20");
+    player.getInventory().clear();
+    player.getInventory().setItem (0,new org.bukkit.inventory.ItemStack (org.bukkit.Material.SNOW_BLOCK,16) );
+    player.getInventory().setItem (1,new org.bukkit.inventory.ItemStack (org.bukkit.Material.DIAMOND_SHOVEL,1) );
+    if (((exports.gameStarted) == (null))){
+      exports.gameStarted=1;
+      var manager = org.bukkit.Bukkit.getScoreboardManager();
+      exports.board = manager.getNewScoreboard();
+      var objective = exports.board.registerNewObjective("objective1", "HEALTH", "Scoreboard");
+      objective.setDisplaySlot(org.bukkit.scoreboard.DisplaySlot.SIDEBAR);
+      objective = exports.board.getObjective (org.bukkit.scoreboard.DisplaySlot.SIDEBAR);
+      objective.setDisplayName("Welcome to Spleef yo");
+      // Update scoreboard for all players
       players=server.getOnlinePlayers();
-      for (var i=0; i<players.length;i++) {
-        (function() {
-          if (players[i] != null ) {
-             players[i].sendMessage (players[i].name + " died");
+      for (var playerIndex=0; playerIndex<players.length;playerIndex++) {
+        score=(players[playerIndex]== null)? null : (players[playerIndex].getMetadata == null)?null:(players[playerIndex].getMetadata("_score_").length == 0)?null:players[playerIndex].getMetadata("_score_")[0].value();
+        objective = exports.board.getObjective (org.bukkit.scoreboard.DisplaySlot.SIDEBAR);
+        objective.getScore(players[playerIndex]).setScore(score);
+        players[playerIndex].setScoreboard (exports.board);
+      };
+      org.bukkit.Bukkit.dispatchCommand(org.bukkit.Bukkit.getConsoleSender(), "setworldspawn -73 29 80");
+      org.bukkit.Bukkit.dispatchCommand(org.bukkit.Bukkit.getConsoleSender(), "setspawn -73 29 80");
+    }
+    if (player != null) {
+      objective = exports.board.getObjective (org.bukkit.scoreboard.DisplaySlot.SIDEBAR);
+      objective.getScore(player).setScore(0);
+      player.setScoreboard (exports.board);
+    }
+  });
+  events.playerInteract( function (event) {
+    block=(event.getClickedBlock== null) ? null : event.getClickedBlock();
+    if ((((block==null)?null:block.getType()) == (org.bukkit.Material.OAK_SIGN))){
+      teamColor=(block.state.getLine(1)).toUpperCase();
+      console.log ("got teamColor: " + teamColor);
+    }
+  });
+  events.playerDeath( function (event) {
+    player=(event.getPlayer== null) ? null : event.getPlayer();
+    players=server.getOnlinePlayers();
+    for (var i=0; i<players.length;i++) {
+      (function() {
+        if (players[i] != null ) {
+           players[i].sendMessage (players[i].name + " died");
+        }
+       })();
+      if (((players[i]) != (player))){
+        score=(players[i]== null)? null : (players[i].getMetadata == null)?null:(players[i].getMetadata("_score_").length == 0)?null:players[i].getMetadata("_score_")[0].value();
+        (function () {
+          var value = ( score==null)?0:score;
+          score= value+1;
+        })();
+        fd = new org.bukkit.metadata.FixedMetadataValue (__plugin,score);
+        if (players[i] != null) {
+          if (players[i].setMetadata != null ) {
+            players[i].setMetadata ("_score_", fd );
           }
-         })();
-        if (((players[i]) != (player))){
-          score=(players[i]== null)? null : (players[i].getMetadata == null)?null:(players[i].getMetadata("_score_").length == 0)?null:players[i].getMetadata("_score_")[0].value();
-          (function () {
-            var value = ( score==null)?0:score;
-            score= value+1;
-          })();
-          fd = new org.bukkit.metadata.FixedMetadataValue (__plugin,score);
-          if (players[i] != null) {
-            if (players[i].setMetadata != null ) {
-              players[i].setMetadata ("_score_", fd );
-            }
-          }
+        }
+        if (player != null) {
           objective = exports.board.getObjective (org.bukkit.scoreboard.DisplaySlot.SIDEBAR);
           objective.getScore(players[i]).setScore(score);
           players[i].setScoreboard (exports.board);
         }
-      };
-    });
-    events.playerRespawn( function (event) {
-      player=(event.getPlayer== null) ? null : event.getPlayer();
-      org.bukkit.Bukkit.dispatchCommand(org.bukkit.Bukkit.getConsoleSender(), "effect give " + player.name + " instant_health 20");
-      player.getInventory().clear();
-      player.setGameMode(org.bukkit.GameMode.SURVIVAL);
-      player.getInventory().setItem (0,new org.bukkit.inventory.ItemStack (org.bukkit.Material.SNOW_BLOCK,16) );
-      player.getInventory().setItem (1,new org.bukkit.inventory.ItemStack (org.bukkit.Material.DIAMOND_SHOVEL,1) );
-      setTimeout (function () {
-        player.teleport(new org.bukkit.Location(server.worlds[0], -87, 14, 132), org.bukkit.event.player.PlayerTeleportEvent.TeleportCause.PLUGIN);
-      },2000);
-    });
-    events.blockBreak( function (event) {
-      block=event.getBlock();
-      if ((((block==null)?null:block.getType()) != (org.bukkit.Material.SNOW_BLOCK))){
-        event.cancelled = true;
       }
-    });
-  }
-};
-
-exports.spleefJoin = function (player) {
-  //Instantiations;
-  var objective;
-  fd = new org.bukkit.metadata.FixedMetadataValue (__plugin,0);
-  if (player != null) {
-    if (player.setMetadata != null ) {
-      player.setMetadata ("_score_", fd );
+    };
+  });
+  events.playerRespawn( function (event) {
+    player=(event.getPlayer== null) ? null : event.getPlayer();
+    org.bukkit.Bukkit.dispatchCommand(org.bukkit.Bukkit.getConsoleSender(), "effect give " + player.name + " instant_health 20");
+    player.getInventory().clear();
+    player.setGameMode(org.bukkit.GameMode.SURVIVAL);
+    player.getInventory().setItem (0,new org.bukkit.inventory.ItemStack (org.bukkit.Material.SNOW_BLOCK,16) );
+    player.getInventory().setItem (1,new org.bukkit.inventory.ItemStack (org.bukkit.Material.DIAMOND_SHOVEL,1) );
+    setTimeout (function () {
+      player.teleport(new org.bukkit.Location(server.worlds[0], -87, 14, 132), org.bukkit.event.player.PlayerTeleportEvent.TeleportCause.PLUGIN);
+    },2000);
+  });
+  events.blockBreak( function (event) {
+    block=event.getBlock();
+    if ((((block==null)?null:block.getType()) != (org.bukkit.Material.SNOW_BLOCK))){
+      event.cancelled = true;
     }
-  }
-  setTimeout (function () {
-    player.teleport(new org.bukkit.Location(server.worlds[0], -87, 14, 132), org.bukkit.event.player.PlayerTeleportEvent.TeleportCause.PLUGIN);
-  },2000);
-  player.setGameMode(org.bukkit.GameMode.SURVIVAL);
-  org.bukkit.Bukkit.dispatchCommand(org.bukkit.Bukkit.getConsoleSender(), "effect give " + player.name + " instant_health 20");
-  player.getInventory().clear();
-  player.getInventory().setItem (0,new org.bukkit.inventory.ItemStack (org.bukkit.Material.SNOW_BLOCK,16) );
-  player.getInventory().setItem (1,new org.bukkit.inventory.ItemStack (org.bukkit.Material.DIAMOND_SHOVEL,1) );
-  spleef();
-  objective = exports.board.getObjective (org.bukkit.scoreboard.DisplaySlot.SIDEBAR);
-  objective.getScore(player).setScore(0);
-  player.setScoreboard (exports.board);
+  });
 };
